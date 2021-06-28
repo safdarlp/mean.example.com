59a9e74b11921916474ff2a58e90a05591fc021f
SAFDAR KHAN <SAFDARLP@GMAIL.COM>
2021-06-28 14:01:54 -0500

    Add logout methods

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('auth/index', { title: 'User Authentication' });
 });
+//~line 7
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth');
+});
 
 module.exports = router;
