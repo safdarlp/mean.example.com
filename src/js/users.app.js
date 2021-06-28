0b7a2922e5af28f11020da9ba92b8ddf5492b113
SAFDAR KHAN <SAFDARLP@GMAIL.COM>
2021-06-28 15:47:21 -0500

    Add a common method for processing web forms

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -60,6 +60,88 @@ var usersApp = (function() {
       }
     }
   
+    function createUser(){
+      var app = document.getElementById('app');
+  
+      var form =  `
+          <div class="card">
+            <div class="card-header clearfix">
+              <h2 class="h3 float-left">Create a New User</h2>
+              <div class="float-right">
+                <a href="#" class="btn btn-primary">Cancel</a>
+              </div>
+            </div>
+            <div class="card-body">
+              <form id="createUser" class="card-body">
+                <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+  
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="first_name">First Name</label>
+                    <input type="text" id="first_name" name="first_name" class="form-control" required>
+                  </div>
+  
+                  <div class="form-group col-md-6">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" id="last_name" name="last_name" class="form-control" required>
+                  </div>
+                </div>
+  
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                  </div>
+  
+                  <div class="form-group col-md-6">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                  </div>
+                </div>
+  
+                <div class="text-right">
+                  <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+                </div>
+              </form>
+            </div>
+          </div>
+      `;
+  
+      app.innerHTML=form;
+    }
+  
+    function postRequest(formId, url){
+      let form = document.getElementById(formId);
+      form.addEventListener('submit', function(e){
+        e.preventDefault();
+  
+        let formData = new FormData(form);
+        let uri = `${window.location.origin}${url}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', uri);
+  
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+  
+        let object = {};
+        formData.forEach(function(value, key){
+          object[key]=value;
+        });
+  
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          if(data.success===true){
+            window.location.hash = `#view-${data.user._id}`
+          }else{
+            document.getElementById('formMsg').style.display='block';
+          }
+        }
+      });
+    }
+  
     return {
       load: function(){
         let hash = window.location.hash;
@@ -67,7 +149,8 @@ var usersApp = (function() {
   
         switch(hashArray[0]){
           case '#create':
-            console.log('CREATE');
+            createUser();
+            postRequest('createUser', '/api/users');
             break;
   
           case '#view':
